23829c226cad0c74925b679bda80e3abeeb2fb46
kylieyoo55 <60900990+kylieyoo55@users.noreply.github.com>
2020-12-30 15:14:17 +1300

    Bcrypted password, and salted X10

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const express = require("express");
 const ejs=require("ejs");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
-const md5 = require("md5");
-
-// const encrypt = require("mongoose-encryption");
+const bcrypt =require("bcrypt");
+const saltRounds =10;
 
 const app=express();
 
@@ -22,9 +21,6 @@ const userSchema= new mongoose.Schema({
 });
 
 
-//conveniente secret encrypted
-// const secret = process.env.SECRET;
-// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 //model
 const User=new mongoose.model("User",userSchema);
@@ -46,20 +42,31 @@ get(
 )
 .post(
 (req,res)=>{
-    const username = req.body.username;
-    const password = md5(req.body.password);
+    const username=req.body.username;
+    const password=req.body.password;
+    // Load hash from your password DB.
 
-    User.findOne({email:username},(err,foundUser)=>{
-if(err){console.log(err)}
-else{
-    if(foundUser){
-        if(foundUser.password === password){
-            res.render("secrets")
-        }
-    }
+    
+        
+            User.findOne({email:username},(err,foundUser)=>{
+                if(err){console.log(err)}
+                else{
+                    if(foundUser){
+
+                        bcrypt.compare(password, foundUser.password, function(err, result) {
+if(result == true){
+    res.render("secrets")
 }
 
-    })
+                        });
+                        
+                            
+                        
+                    }
+                }
+        })
+
+  
 }
 
 )
@@ -74,14 +81,18 @@ app.route("/register")
     }
 )
 .post((req,res)=>{
-    const newUser =new User({
-email:req.body.username,
-password:md5(req.body.password)
-})
-newUser.save((err)=>{
-    if(!err){res.render("secrets")}
-    else{res.send(err)};
-})
+    bcrypt.hash(req.body.password,saltRounds, function(err, hash) {
+        const newUser =new User({
+            email:req.body.username,
+            password:hash
+            })
+            newUser.save((err)=>{
+                if(!err){res.render("secrets")}
+                else{res.send(err)};
+            })
+    });
+
+ 
 })
 
 
